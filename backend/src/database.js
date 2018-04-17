53f0dc947080bcd0a2937d4722df1a3365008c50
Marko Klemetti <marko.klemetti@gmail.com>
2018-04-17 10:42:29 +0300

    Simplify file structure

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,11 +1,37 @@
-const sequelize = require('./models/sequelize');
+var Sequelize = require('sequelize');
 
-exports.Chat = require('./models/chat');
+/***** INIT DATABASE *****/
+
+const sequelize = new Sequelize({
+  logging: false,
+  dialect: 'sqlite',
+  storage: './db/chat.sqlite',
+});
+
+/***** INIT CHAT TABLE WITH MESSAGE *****/
+
+const Chat = sequelize.define('chats', {
+  message: Sequelize.TEXT,
+}, {
+  timestamps: true,
+  instanceMethods: {
+    toJSON: async function () {
+      return {
+        // Id and timestamps are generated automatically
+        id: this.id,
+        createdAt: this.createdAt,
+
+        // Message was added on the POST request
+        message: this.message,
+      };
+    },
+  },
+});
+
+/***** EXPORT CHAT OBJECT *****/
 
 exports.sync = (options) => {
   return sequelize.sync(options);
 };
 
-exports.transaction = (options) => {
-  return sequelize.transaction(options);
-};
+exports.Chat = Chat;
--- backend/src/models/sequelize.js
+++
@@ -1,9 +0,0 @@
-var Sequelize = require('sequelize');
-
-var sequelize = new Sequelize({
-  logging: false,
-  dialect: 'sqlite',
-  storage: './db/chat.sqlite',
-});
-
-module.exports = sequelize;
